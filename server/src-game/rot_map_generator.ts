7d57667146ec235cf5daa50c0a5f70b504c93829
ooz <ooz@users.noreply.github.com>
2025-01-26 15:03:42 +0100

    Add switches

--- server/src-game/rot_map_generator.ts
+++ server/src-game/rot_map_generator.ts
@@ -2,6 +2,13 @@ import * as ROT from "../lib/rot.js"
 import { MANIFEST } from "./manifest.js"
 import { Map, tiles_create } from "./map.js"
 
+const ENTITY_ID2SWITCH_ID = {
+    "0": "switchA",
+    "1": "switchB",
+    "2": "switchC",
+    "3": "switchD"
+}
+
 export function generate_rogue_dungeon(numberOfPlayers: number) {
     const rotMap = new ROT.Map.Rogue(32, 32)
     //console.log("Rot map: %o", rotMap)
@@ -52,6 +59,18 @@ export function generate_rogue_dungeon(numberOfPlayers: number) {
         if (tile.type === MANIFEST.tiles.water) {
             map.setTile(x, y, MANIFEST.tiles.oxygen)
             bubblesToSpawn--;
+        }
+    }
+
+    let switchesToSpawn = numberOfPlayers
+    while (switchesToSpawn > 0) {
+        const x = getRandomInt(32)
+        const y = getRandomInt(32)
+        const tile = map.getTile(x, y)
+        const switchId = ENTITY_ID2SWITCH_ID[String(switchesToSpawn - 1)]
+        if (tile.type === MANIFEST.tiles.water) {
+            map.setTile(x, y, MANIFEST.tiles[switchId])
+            switchesToSpawn--;
         }
     }
 
